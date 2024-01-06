2ef9d11c44fda237754802145140d76811aaad07
ye0jin <fruitfighterlim@gmail.com>
2024-01-07 00:31:32 +0900

    사운드+esc

---
+++ AppJam7/Assets/01_Scripts/Manager/SoundManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    private AudioSource audioSource;
+    private AudioSource bgmAudioSource;
+
+    private string bgmKey = "BGMVolume";
+    private string sfxKey = "SFXVolume";
+
+    [Header("BGM")]
+    [Header("Player")]
+    [Header("Obstacle")]
+    public AudioClip glassSound;
+
+    private void Awake()
+    {
+        if (Instance != null) Debug.LogError("Soundmanager error");
+        Instance = this;
+
+        // audioSource 달아주세요!!^_^
+        bgmAudioSource = Camera.main.GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
+
+        float bgmVolume = PlayerPrefs.GetFloat(bgmKey, 0.1f);
+        float sfxVolume = PlayerPrefs.GetFloat(sfxKey, 0.4f);
+        bgmAudioSource.volume = bgmVolume;
+        audioSource.volume = sfxVolume;
+    }
+
+    private void Start()
+    {
+        bgmAudioSource.Play();
+        bgmAudioSource.loop = true;
+    }
+
+
+    // 볼륨 조절 호출 함수
+    public void SetBGMVolume(float value)
+    {
+        bgmAudioSource.volume = value;
+        PlayerPrefs.SetFloat(bgmKey, value);
+    }
+    public void SetSFXVolume(float value)
+    {
+        audioSource.volume = value;
+        PlayerPrefs.SetFloat(sfxKey, value);
+    }
+
+    //SoundManager.Instance.PlayCardSelectSound(); <- 이런식으로 사용
+    public void PlayCardSelectSound()
+    {
+        audioSource.PlayOneShot(glassSound);
+    }
+}
--- AppJam7/Assets/01_Scripts/Manager/UIManager.cs
+++ AppJam7/Assets/01_Scripts/Manager/UIManager.cs
@@ -5,12 +5,73 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("ESC")]
+    private bool isSetting = false;
+    [SerializeField] private Transform settingPanel;
+
+    [Header("ESC - SoundUI")]
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    private string bgmKey = "BGMVolume";
+    private string sfxKey = "SFXVolume";
+
+    [Header("Gauge")]
     [SerializeField] private Image dashGauge;
     [SerializeField] private Image painGauge;
 
+
+    private void Start()
+    {
+        isSetting = false;
+        settingPanel.gameObject.SetActive(false);
+
+        float bgmVolume = PlayerPrefs.GetFloat(bgmKey);
+        float sfxVolume = PlayerPrefs.GetFloat(sfxKey);
+
+        bgmSlider.value = bgmVolume;
+        sfxSlider.value = sfxVolume;
+    }
+
     private void Update()
     {
-        SetUI();
+        //SetUI();
+        SetEscPanel();
+    }
+
+    public void SetEscPanel()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isSetting)
+            {
+                Time.timeScale = 1;
+                isSetting = false;
+                settingPanel.gameObject.SetActive(false);
+            }
+            else
+            {
+                Time.timeScale = 0;
+                isSetting = true;
+                settingPanel.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void BGMSliderChangedVolume()
+    {
+        float bgmVolume = bgmSlider.value;
+
+        SoundManager.Instance.SetBGMVolume(bgmVolume);
+        PlayerPrefs.SetFloat(bgmKey, bgmVolume);
+    }
+
+    public void SFXSliderChangedVolume()
+    {
+        float sfxVolume = sfxSlider.value;
+
+        SoundManager.Instance.SetSFXVolume(sfxVolume);
+        PlayerPrefs.SetFloat(sfxKey, sfxVolume);
     }
 
     private void SetUI()
