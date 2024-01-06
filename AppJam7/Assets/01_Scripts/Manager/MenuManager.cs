6add4a8bfd5e8ee45ac523fdbba51e1de16ad7f1
LeeKiHwan <s22049@gsm.hs.kr>
2024-01-06 16:15:39 +0900

    GameManager, MenuManager 클래스 생성

---
+++ AppJam7/Assets/01_Scripts/Manager/MenuManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
