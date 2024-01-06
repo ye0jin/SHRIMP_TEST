866c7ea6a349bb549c938a102bd958498bdfb3ad
ye0jin <fruitfighterlim@gmail.com>
2024-01-07 02:17:26 +0900

    사운드랑 유아이

--- AppJam7/Assets/01_Scripts/Manager/UIManager.cs
+++ AppJam7/Assets/01_Scripts/Manager/UIManager.cs
@@ -9,6 +9,17 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
 
+    [Header("ESC")]
+    private bool isSetting = false;
+    [SerializeField] private Transform settingPanel;
+
+    [Header("ESC - SoundUI")]
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    private string bgmKey = "BGMVolume";
+    private string sfxKey = "SFXVolume";
+
     [SerializeField] private Image fade;
     [SerializeField] private Image dashGauge;
     [SerializeField] private Image painGauge;
@@ -21,10 +32,57 @@ public class UIManager : MonoBehaviour
     private void Start()
     {
         FadeIn(0);
+
+
+        isSetting = false;
+        settingPanel.gameObject.SetActive(false);
+
+        float bgmVolume = PlayerPrefs.GetFloat(bgmKey);
+        float sfxVolume = PlayerPrefs.GetFloat(sfxKey);
+
+        bgmSlider.value = bgmVolume;
+        sfxSlider.value = sfxVolume;
     }
+
     private void Update()
     {
         SetUI();
+        SetEscPanel();
+    }
+
+    public void SetEscPanel()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isSetting)
+            {
+                Time.timeScale = 1;
+                isSetting = false;
+                settingPanel.gameObject.SetActive(false);
+            }
+            else
+            {
+                Time.timeScale = 0;
+                isSetting = true;
+                settingPanel.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void BGMSliderChangedVolume()
+    {
+        float bgmVolume = bgmSlider.value;
+
+        SoundManager.Instance.SetBGMVolume(bgmVolume);
+        PlayerPrefs.SetFloat(bgmKey, bgmVolume);
+    }
+
+    public void SFXSliderChangedVolume()
+    {
+        float sfxVolume = sfxSlider.value;
+
+        SoundManager.Instance.SetSFXVolume(sfxVolume);
+        PlayerPrefs.SetFloat(sfxKey, sfxVolume);
     }
 
     public void FadeIn(float value)
