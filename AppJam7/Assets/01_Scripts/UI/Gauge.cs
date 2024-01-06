104c533c7746574dad76ff8542fe339a330ee0f2
ye0jin <fruitfighterlim@gmail.com>
2024-01-06 15:25:17 +0900

    ads

---
+++ AppJam7/Assets/01_Scripts/UI/Gauge.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gauge : MonoBehaviour
+{
+    //[SerializeField] pri
+
+    /*public IEnumerator GaugeImage()
+    {
+
+    }*/
+}
